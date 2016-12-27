ce850cf60eadb75e7c56ae9a3159c872016d28b0
laozhang.gz <laozhang.gz@gmail.com>
2016-12-27 10:45:37 +0800

    roll browser window.

---
+++ codesource/yidascan/OpcHelper.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ProduceComm.OPC;
+using yidascan.DataAccess;
+
+namespace yidascan {    
+    public static class OpcHelper {
+        static OPCClient client = null;
+        static OPCParam param = null;
+        
+        /// <summary>
+        /// 读布尔值。
+        /// </summary>
+        /// <param name="slot">地址</param>
+        /// <returns>非bool值时，弹出异常。</returns>
+        public static bool ReadBool(string slot) {
+            return (bool)client.Read(slot);
+        }
+
+        public static string ReadString(string slot) {
+            return (string)client.Read(slot);
+        }
+
+        public static decimal ReadDecimal(string slot) {
+            return (decimal)client.Read(slot);
+        }
+
+        public static  int ReadInt(string slot) {
+            return (int)client.Read(slot);            
+        }
+
+        public static string ReadACAreaLabel(LCodeSignal signal) {
+            const int MAX_LEN = 6;
+            string r1 = ReadString(signal.LCode1);
+            string r2 = ReadString(signal.LCode2);
+            return r1.PadLeft(MAX_LEN, '0') + r2.PadLeft(MAX_LEN, '0');            
+        }
+
+        public static int ReadWeighingSignal(string slot) {
+            return ReadInt(param.ScanParam.GetWeigh);
+        }
+
+        public static void WriteWeighingSignal(bool b) {
+            var val = b ? "0" : "2";
+            client.Write(param.ScanParam.GetWeigh, val);
+        }
+
+        public static void WriteACAreaCompletionSignal(LCodeSignal signal) {
+            client.Write(signal.Signal, 0);
+        }
+
+        public static string ReadCacheLabel() {
+            const int MAX_LEN = 6;
+            string r1 = ReadString(param.CacheParam.BeforCacheLable1);
+            string r2 = ReadString(param.CacheParam.BeforCacheLable2);
+            return r1.PadLeft(MAX_LEN, '0') + r2.PadLeft(MAX_LEN, '0');
+        }
+
+        /// <summary>
+        /// 读布卷到达缓冲信号。
+        /// </summary>
+        /// <returns></returns>
+        public static bool ReadBeforeCacheSignal() {
+            return ReadBool(param.CacheParam.BeforCacheStatus);            
+        }
+
+        public static void ResetBeforeCacheSignal() {
+            client.Write(param.CacheParam.BeforCacheStatus, false);
+        }
+
+        /// <summary>
+        /// 读扫描状态信号。
+        /// </summary>
+        /// <returns></returns>
+        public static bool ReadScanState() {
+            return ReadBool(param.ScanParam.ScanState);            
+        }
+
+        /// <summary>
+        /// 标签扫描处理完成以后，发消息告知OPC。
+        /// </summary>
+        /// <param name="area">交地所在的区代码："A", "B" or "C"</param>
+        /// <param name="numb">交地序号</param>
+        /// <param name="label1">标签前半部分</param>
+        /// <param name="label2">标签后半部分</param>
+        /// <param name="camera">相机号</param>
+        public static void WriteScanOK(string area, string numb, string label1, string label2, string camera) {
+            // 交地
+            client.Write(param.ScanParam.ToLocationArea, area);
+            client.Write(param.ScanParam.ToLocationNo, numb);
+            // 标签条码
+            client.Write(param.ScanParam.ScanLable1, label1);
+            client.Write(param.ScanParam.ScanLable2, label2);
+            // 相机
+            client.Write(param.ScanParam.CameraNo, camera);
+            // 完成信号。
+            WriteScanOK();
+        }
+
+        /// <summary>
+        /// 布卷条码扫描完成，令托架放下，传送带动。
+        /// </summary>
+        public static void WriteScanOK() {
+            client.Write(param.ScanParam.ScanState, true);
+        }
+    }
+}
---
+++ codesource/yidascan/WRollBrowser.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace yidascan {
+    partial class WRollBrowser {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WRollBrowser));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.btnExit = new System.Windows.Forms.ToolStripButton();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbxPanelList = new System.Windows.Forms.ListBox();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lbxCache = new System.Windows.Forms.ListBox();
+            this.lbxPanel = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnExit});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(630, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 395);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(630, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // btnExit
+            // 
+            this.btnExit.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
+            this.btnExit.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(36, 22);
+            this.btnExit.Text = "退出";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 25);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.lbxPanelList);
+            this.splitContainer1.Panel1.Controls.Add(this.panel1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(630, 370);
+            this.splitContainer1.SplitterDistance = 210;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(210, 27);
+            this.panel1.TabIndex = 0;
+            // 
+            // lbxPanelList
+            // 
+            this.lbxPanelList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbxPanelList.FormattingEnabled = true;
+            this.lbxPanelList.ItemHeight = 12;
+            this.lbxPanelList.Location = new System.Drawing.Point(0, 27);
+            this.lbxPanelList.Name = "lbxPanelList";
+            this.lbxPanelList.Size = new System.Drawing.Size(210, 343);
+            this.lbxPanelList.TabIndex = 1;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.lbxCache);
+            this.splitContainer2.Panel1.Controls.Add(this.panel2);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.lbxPanel);
+            this.splitContainer2.Panel2.Controls.Add(this.panel3);
+            this.splitContainer2.Size = new System.Drawing.Size(416, 370);
+            this.splitContainer2.SplitterDistance = 138;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(416, 27);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(416, 27);
+            this.panel3.TabIndex = 1;
+            // 
+            // lbxCache
+            // 
+            this.lbxCache.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbxCache.FormattingEnabled = true;
+            this.lbxCache.ItemHeight = 12;
+            this.lbxCache.Location = new System.Drawing.Point(0, 27);
+            this.lbxCache.Name = "lbxCache";
+            this.lbxCache.Size = new System.Drawing.Size(416, 111);
+            this.lbxCache.TabIndex = 2;
+            // 
+            // lbxPanel
+            // 
+            this.lbxPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbxPanel.FormattingEnabled = true;
+            this.lbxPanel.ItemHeight = 12;
+            this.lbxPanel.Location = new System.Drawing.Point(0, 27);
+            this.lbxPanel.Name = "lbxPanel";
+            this.lbxPanel.Size = new System.Drawing.Size(416, 201);
+            this.lbxPanel.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "板列表";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(416, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "缓存状态";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(416, 27);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "码垛状态";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // WRollBrowser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 417);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "WRollBrowser";
+            this.Text = "布卷状态浏览";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnExit;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListBox lbxPanelList;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.ListBox lbxCache;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListBox lbxPanel;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
